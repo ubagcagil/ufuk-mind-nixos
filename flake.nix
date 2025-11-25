c76285348644dd56edd967ef85ac0a56e6cbc1bc
Ufuk Berk Agcagil <ufuk@nixos.local>
2025-11-25 19:47:33 +0300

    Split desktop config into host module and flake profile

--- flake.nix
+++ flake.nix
@@ -4,12 +4,17 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
 
   outputs = { self, nixpkgs, ... }: {
-    nixosConfigurations.ufuk-desktop = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ./configuration.nix
-      ];
+    nixosConfigurations = {
+      ufuk-desktop = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hosts/ufuk-desktop.nix
+        ];
+      };
+
+      # TODO: ufuk-laptop:
+      # Buraya daha sonra laptop donanım modülü ile ikinci profil gelecek.
+      # ufuk-laptop = nixpkgs.lib.nixosSystem { ... };
     };
   };
 }
-
---
+++ hosts/ufuk-desktop.nix
@@ -0,0 +1,88 @@
+{ config, pkgs, ... }:
+
+{
+  ################################
+  # Donanım + ortak modül
+  ################################
+  imports = [
+    ../hardware-configuration.nix
+    ../modules/common.nix
+  ];
+
+  ################################
+  # Hostname
+  ################################
+  networking.hostName = "ufuk-desktop";
+
+  ################################
+  # NAT: wg0 -> enp5s0 üzerinden internete çıksın
+  ################################
+  networking.nat = {
+    enable = true;
+    externalInterface = "enp5s0";
+    internalInterfaces = [ "wg0" ];
+  };
+
+  # Ek garanti: IPv4 forwarding
+  boot.kernel.sysctl."net.ipv4.ip_forward" = 1;
+
+  ################################
+  # NVIDIA / RTX 4090
+  ################################
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = false;
+    open = false;          # 4090 için proprietary driver
+    nvidiaSettings = true; # nvidia-settings aracı
+  };
+
+  ################################
+  # WireGuard: ufuk-desktop (server)
+  ################################
+  networking.wireguard.enable = true;
+  networking.wireguard.interfaces.wg0 = {
+    ips = [ "10.10.0.1/24" ];
+    listenPort = 51820;
+    privateKeyFile = "/etc/wireguard/wg0-privkey";
+
+    peers = [
+      {
+        # Laptop peer
+        publicKey = "xMFEFKGQL6Hi46xT/OzKD6wyEbG7r9dMUPEackQO1Xo=";
+        allowedIPs = [ "10.10.0.2/32" ];
+        persistentKeepalive = 25;
+      }
+    ];
+  };
+
+  ################################
+  # Güç yönetimi – kasa ASLA uyumaya gitmesin
+  ################################
+  systemd.sleep.extraConfig = ''
+    AllowSuspend=no
+    AllowHibernation=no
+    AllowHybridSleep=no
+    AllowSuspendThenHibernate=no
+  '';
+
+  services.logind = {
+    lidSwitch = "ignore";         # laptop için de olsa zararı yok
+    lidSwitchDocked = "ignore";
+    extraConfig = ''
+      IdleAction=ignore
+      IdleActionSec=0
+    '';
+  };
+
+  ################################
+  # State version
+  ################################
+  system.stateVersion = "25.05";
+}
